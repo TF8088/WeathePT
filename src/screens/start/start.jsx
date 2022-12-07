70d18a89943292e0ba0eb25b830904908deee0b5
TF8088 <tiagofarinha03@outlook.com>
2022-12-07 15:58:14 +0000

    erro: code erro

--- src/screens/start/start.jsx
+++ src/screens/start/start.jsx
@@ -2,10 +2,12 @@ import { View, Text, Image } from "react-native";
 import style from "./start-style";
 
 export function start() {
-    <View style={style.container}>
-        <Image source={require('../../assets/weatherlogo.png')} style={style.logo} />
-        <Text style={style.logoText}>
-            Portugal Weather
-        </Text>
-    </View>
+    return (
+        <View style={style.container}>
+            <Image source={require('../../assets/weatherlogo.png')} style={style.logo} />
+            <Text style={style.logoText}>
+                Portugal Weather
+            </Text>
+        </View>
+    )
 }
