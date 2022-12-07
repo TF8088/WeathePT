f24645fb2de483faa29c1c383f235029dbf1eea3
TF8088 <tiagofarinha03@outlook.com>
2022-12-07 15:51:39 +0000

    route system: add start route

---
+++ src/screens/start/start-style.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from "react-native";
+
+const style = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#5257F2',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    logo: {
+        width: 200,
+        height: 200,
+    },
+    logoText: {
+        color: '#FFFFFF',
+        fontSize: 30,
+        fontWeight: '700',
+        marginTop: 30,
+        letterSpacing: 3,
+    }   
+})
+
+export default style    
