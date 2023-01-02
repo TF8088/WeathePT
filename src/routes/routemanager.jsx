18f831f0f7f7e19870b9032276077b1189936c93
TF8088 <tiagofarinha03@outlook.com>
2023-01-02 11:50:37 +0000

    route system: added home route

--- src/routes/routemanager.jsx
+++ src/routes/routemanager.jsx
@@ -1,26 +1,27 @@
 import React, { useEffect , useState } from "react";
+import { View, Text } from 'react-native';
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Start } from "../screens/start/start";
+import { Home } from "../screens/home/home";
 
 export default function RouteManager() {
     const [firststart, setfirststart] = useState(Boolean);
     useEffect(() => {
         AsyncStorage.getItem('@firststart').then(value => {
             if (value === null) /* Never was Opened */  {
-                AsyncStorage.setItem('@firststart', value = 'true'); 
+                AsyncStorage.setItem('@firststart', 'true'); 
                 setfirststart(true);
             } else /* Was Opened */ {
                 setfirststart(false);
             }
-        })
-    }, [])
+        }).catch((err) => {console.error(err)});
+    }, []);
      /* Print the status just for tests*/
-     console.log(firststart); 
-
-     if (firststart == true) {
-        /* Start -> First load */
-        return Start();
-    } else {
-        /* Load Home Page */
-        return Start();
-    }
+    // console.log(firststart); 
+      if (firststart == true) {
+         /* Start -> First load */
+         return <Start />;
+     } else {
+         return <Home />;
+     }
 }
